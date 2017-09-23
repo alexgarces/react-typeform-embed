81b9c3a6a7a1243694ae49dd4f9021c4746b6a21
Alejandro Garces Silla <info@alexgarces.com>
2017-09-23 20:30:47 +0200

    Add typeform/embed

--- src/App.js
+++ src/App.js
@@ -1,18 +1,37 @@
 import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import * as typeformEmbed from '@typeform/embed';
+import moment from 'moment';
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    };
+  }
+
+  componentDidMount() {
+    const elm = this.refs.typeform;
+    console.log('typeformEmbed',typeformEmbed);
+
+    typeformEmbed.makeWidget(elm, 'https://developerplatform.typeform.com/to/Xc7NMh');
+  }
+
   render() {
+    console.log('moment',moment);
+
     return (
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome to React</h2>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div ref="typeform" style={{height: 1000}}>
+
+        </div>
       </div>
     );
   }
