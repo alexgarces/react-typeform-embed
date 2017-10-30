16772dafbd1ffd0bb389ab276cad55d092828a9c
Alejandro Garces Silla <info@alexgarces.com>
2017-10-30 22:02:13 +0100

    Remove useless constructor

--- src/examples/ExampleWidget.js
+++ src/examples/ExampleWidget.js
@@ -1,18 +1,12 @@
 import React from 'react';
 import { ReactTypeformEmbed } from '../components';
 
-class ExampleWidget extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-
-  render() {
-    return (
-      <div className="ExampleWidget">
-        <ReactTypeformEmbed popup={false} url={'https://demo.typeform.com/to/njdbt5'}/>
-      </div>
-    );
-  }
+const ExampleWidget = () => {
+  return (
+    <div className="ExampleWidget">
+      <ReactTypeformEmbed popup={false} url={'https://demo.typeform.com/to/njdbt5'}/>
+    </div>
+  );
 }
 
 export default ExampleWidget;
