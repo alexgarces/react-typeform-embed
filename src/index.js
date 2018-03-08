94282121ff9be6dbe5868d6e63270aa3f2bfc54a
Alejandro Garces Silla <info@alexgarces.com>
2018-03-08 15:56:25 +0100

    Remove registerServiceWorker import

--- src/index.js
+++ src/index.js
@@ -3,6 +3,5 @@ import ReactDOM from 'react-dom';
 import './bootstrap.min.css';
 import './index.css';
 import Examples from './examples';
-import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<Examples />, document.getElementById('root'));
