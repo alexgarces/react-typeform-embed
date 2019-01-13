6b1750bb855622b78a004c84b9d0796326abe188
Alejandro Garces Silla <info@alexgarces.com>
2019-01-13 16:58:14 +0100

    Updates

---
+++ precache-manifest.2ce5b829bee5a3237bb6eb6d02700d6a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d4cc2fdec2a68978b3bc",
+    "url": "/react-typeform-embed/static/css/main.05c7ed5c.chunk.css"
+  },
+  {
+    "revision": "d4cc2fdec2a68978b3bc",
+    "url": "/react-typeform-embed/static/js/main.d4cc2fde.chunk.js"
+  },
+  {
+    "revision": "a6e988fbdd4348ea4e3c",
+    "url": "/react-typeform-embed/static/js/1.a6e988fb.chunk.js"
+  },
+  {
+    "revision": "92b82799db8814ff6a34",
+    "url": "/react-typeform-embed/static/js/runtime~main.92b82799.js"
+  },
+  {
+    "revision": "c00f1b97465b913fefb0808f88a3cff6",
+    "url": "/react-typeform-embed/index.html"
+  }
+];
